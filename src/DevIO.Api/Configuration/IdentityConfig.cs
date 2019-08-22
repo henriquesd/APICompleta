11f856731c25a3a13938e2da2902f9d64c4bfcb1
henriquesd <hsd.foz@gmail.com>
2019-08-22 17:01:05 -0300

    Customizando os erros do Identity

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Api.Data;
+using DevIO.Api.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -17,6 +18,7 @@ namespace DevIO.Api.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders(); // este é para adicionar o recurso para poder gerar tokens, por exemplo, para resetar de senhas, para fazer autenticação de e-mail...;
 
             return services;
