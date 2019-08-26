1dd07d9cf4d34c827aad9c97f36670185fc36127
henriquesd <hsd.foz@gmail.com>
2019-08-26 20:25:19 -0300

    Implementando o Logging

--- src/DevIO.Api/V1/Controllers/AuthController.cs
+++ src/DevIO.Api/V1/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using DevIO.Api.ViewModels;
 using DevIO.Business.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
@@ -26,17 +27,19 @@ namespace DevIO.Api.V1.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly AppSettings _appSettings;
+        private readonly ILogger _logger;
 
         public AuthController(INotificador notificador,
                             SignInManager<IdentityUser> signInManager,
                             UserManager<IdentityUser> userManager,
                             IOptions<AppSettings> appSettings,
-                            IUser user) : base(notificador, user)
+                            IUser user, ILogger<AuthController> logger) : base(notificador, user)
         {
             // O IOptions é para pegar dados que servem de parâmetro;
 
             _signInManager = signInManager;
             _userManager = userManager;
+            _logger = logger;
             _appSettings = appSettings.Value;
         }
 
@@ -81,6 +84,7 @@ namespace DevIO.Api.V1.Controllers
 
             if (result.Succeeded)
             {
+                _logger.LogInformation("Usuario " + loginUser.Email + " logado com sucesso");
                 return CustomResponse(await GerarJwt(loginUser.Email));
             }
             if (result.IsLockedOut)
--- src/DevIO.Api/V2/Controllers/TesteController.cs
+++ src/DevIO.Api/V2/Controllers/TesteController.cs
@@ -1,6 +1,7 @@
 ﻿using DevIO.Api.Controllers;
 using DevIO.Business.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DevIO.Api.V2.Controllers
 {
@@ -8,13 +9,30 @@ namespace DevIO.Api.V2.Controllers
     [Route("api/v{version:apiVersion}/teste")]
     public class TesteController : MainController
     {
-        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser)
+        private readonly ILogger _logger;
+
+        // ILogger<TesteController> logger - TesteController = objeto de quem está gerando o Log;
+        public TesteController(INotificador notificador, IUser appUser, ILogger<TesteController> logger) : base(notificador, appUser)
         {
+            _logger = logger;
         }
 
         [HttpGet]
         public string Valor()
         {
+            // estes dois primeiros devem ser utilizados apenas durante o desenvolvimento;
+
+            // O log de Trace ele não gera porque a Microsoft desabilitou por padrão no ASP.NET Core, justamente para evitar que suba em produção um Trace que gera muito volume de dados,
+            // mas é possível habilitar ele manualmente; porém se precisar fazer um Trace, o Information serve para isto também; Pode utilizar o information porque ele aceita qualquer coisa;
+            _logger.LogTrace("Log de Trace"); // é para desenvolvimento; por exemplo, para marcar a hora que o processo começou e a hora que terminou;
+            _logger.LogDebug("Log de Debug"); // informações de debug;
+
+            // estes abaixo podem ser utilizados na aplicação;
+            _logger.LogInformation("Log de Informação"); // vai gravar qualquer coisa que queira informar, que não seja nada de importante mas que queira registrar;
+            _logger.LogWarning("Log de Aviso"); // quando existe alguma situação, por exemplo um erro 404, ou alguma coisa que não é um erro mas também não deveria acontecer;
+            _logger.LogError("Log de Erro"); // propriamente o erro;
+            _logger.LogCritical("Log de Problema Critico"); // o critical é alguma coisa mais séria, alguma coisa que ameaça a performance, a saúde da aplicação; o critical é um nível acima do error;
+
             return "Sou a V2";
         }
     }
